927b29617104dd1b8fe76a7db25480258b35e79d
Joe <joecolasurdo@eltoro.com>
2018-02-17 15:06:05 -0800

    Added package doc.

---
+++ cfnhelper/doc.go
@@ -0,0 +1,5 @@
+// Package cfnhelper contains objects which implement the request/response structures and logic required to interact
+// with AWS CloudFormation for custom resource deployment.
+//
+// This is often referred to as "cfn-response", which is the name of the original implementations written in JavaScript.
+package cfnhelper
